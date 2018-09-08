38fafd9e4472b158e805466532f5f88aa897d218
dangngoclinh <dangngoclinh90@gmail.com>
2018-09-09 00:34:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# lazada" 
+"# lazada" 
---
+++ src/Constants.php
@@ -0,0 +1,8 @@
+<?php
+namespace dangngoclinh\lazada\lazop;
+class Constants
+{
+	static $log_level_debug = "DEBUG";
+	static $log_level_info = "INFO";
+	static $log_level_error = "ERROR";
+}
